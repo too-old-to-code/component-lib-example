007d09431224d844ab6748b9ef551130e3ee3976
Andrew Trigg <andrewt51@hotmail.com>
2020-05-16 14:22:21 +0100

    stylesheetinjector

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,4 +1,30 @@
 import CMS from "netlify-cms-app"
-
+import React, { useState, useEffect } from "react"
+import { StyleSheetManager, ThemeProvider } from "styled-components"
 import IndexPagePreview from "./preview-templates/IndexPagePreview"
-CMS.registerPreviewTemplate("index", IndexPagePreview)
+
+import theme from "../theme"
+
+const StylesheetInjector = ({ children }) => {
+  const [iframeRef, setIframeRef] = useState(undefined)
+
+  useEffect(() => {
+    const iframe = document.querySelector("#nc-root iframe")
+    const iframeHeadElem = iframe && iframe.contentDocument.head
+    setIframeRef(iframeHeadElem)
+  })
+
+  return (
+    <>
+      {iframeRef && (
+        <ThemeProvider theme={theme}>
+          <StyleSheetManager target={iframeRef}>{children}</StyleSheetManager>
+        </ThemeProvider>
+      )}
+    </>
+  )
+}
+
+CMS.registerPreviewTemplate("index", props => (
+  <StyleSheetInjector>IndexPagePreview</StylesheetInjector>
+))
